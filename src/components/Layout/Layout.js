73f5acf7db74faed9350126ece1263cc4fbca7d5
rungo <karol.rungo@nokia.com>
2018-05-18 15:05:58 +0200

    burger ingredient component introduced

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Aux from '../../hoc/Aux/Aux';
 
+import classes from './Layout.css'
+
 const layout = (props) => (
   <Aux>
     <div>
       Toolbar, SideDrawer, Backdrop
     </div>
-    <main>
+    <main className={classes.content}>
       {props.children}
     </main>
   </Aux>
