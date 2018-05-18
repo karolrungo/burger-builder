da796f08a76cf66336b77174b86d6f44614512c8
rungo <karol.rungo@nokia.com>
2018-05-18 14:30:57 +0200

    dummy Layout component wrapped with Aux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Dzień dobry!
-      </div>
+      <Layout>
+        <p>no witam</p>
+      </Layout>
     );
   }
 }
