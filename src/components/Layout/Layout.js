7e39e04ae5432c0f9452158a09eafd5623b48f2e
rungo <karol.rungo@nokia.com>
2018-05-23 13:43:55 +0200

    navigation bar finished with mobile menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux/Aux';
 import Toolbar from './../Navigation/Toolbar/Toolbar'
 import SideDrawer from './../Navigation/SideDrawer/SideDrawer'
 
 import classes from './Layout.css'
 
-const layout = (props) => (
-  <Aux>
-    <SideDrawer />
-    <Toolbar />
-    <main className={classes.content}>
-      {props.children}
-    </main>
-  </Aux>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  }
 
-export default layout
+  sideDrawerClosedHandler = () => {
+    this.setState({
+      showSideDrawer: false,
+    })
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {
+        showSideDrawer: !prevState.showSideDrawer
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Aux>
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}/>
+        <Toolbar
+          toggleSideDrawer={this.sideDrawerToggleHandler}
+        />
+        <main className={classes.content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )}
+}
+
+export default Layout
