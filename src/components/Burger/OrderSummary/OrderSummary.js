e922f36b61de825bcce89fd77a2e556efdcdd082
rungo <karol.rungo@nokia.com>
2018-05-23 14:19:28 +0200

    Layout moved from component to containers dir

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Aux from '../../../hoc/Aux/Aux'
 import Button from './../../UI/Button/Button'
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map( ing => {
-      return <li key={ing}>
-        <span style={{textTransform: 'capitalize'}}>
-          {`${ing}:`}
-        </span>
-        {`${props.ingredients[ing]}`}
-      </li>
-    })
+class OrderSummary extends Component {
+  componentWillUpdate() {
+    console.log("ORDER SUMMARY will update")
+  }
 
-  return (
-    <Aux>
-      <h3>Your order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnTypes={'Danger'} clicked={ props.canceled }>CANCEL</Button>
-      <Button btnTypes={'Success'} clicked={ props.continued }>CONTINUE</Button>
-    </Aux>
-  )
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map( ing => {
+        return <li key={ing}>
+          <span style={{textTransform: 'capitalize'}}>
+            {`${ing}:`}
+          </span>
+          {`${this.props.ingredients[ing]}`}
+        </li>
+      })
+
+    return (
+      <Aux>
+        <h3>Your order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p>Current price: <strong>{this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnTypes={'Danger'} clicked={ this.props.canceled }>CANCEL</Button>
+        <Button btnTypes={'Success'} clicked={ this.props.continued }>CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary
+
+export default OrderSummary
