876661eb215a6c786e6b93142f194481cf3ddf97
rungo <karol.rungo@nokia.com>
2018-05-26 06:16:55 +0200

    memory leak in withErrorHandler fix

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,14 +9,14 @@ const withErrorHandler = (WrappedComponent, axios) => {
     }
 
     componentWillMount() {
-      axios.interceptors.response.use(
+      this.respInterceptor = axios.interceptors.response.use(
         resp => resp,
         error => {
           this.setState({error: error})
         }
       )
 
-      axios.interceptors.request.use(
+      this.reqInterceptor = axios.interceptors.request.use(
         req => {
           this.setState({error: null})
           return req
@@ -25,6 +25,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
           this.setState({error: error})
         }
       )
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor)
+      axios.interceptors.response.eject(this.respInterceptor)
     }
 
     clearErrorHandler = () => {
