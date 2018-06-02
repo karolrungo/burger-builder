e3c9addba7eeadde1649283f4b39d565dd1fcc67
rungo <karol.rungo@nokia.com>
2018-06-02 20:44:44 +0200

    navigation change: a tags replaced with NavLinks

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import classes from './NavigationItems.css'
 
 const navigationItems = (props) => (
   <ul className={ classes.NavigationItems }>
-    <NavigationItem link={'/'} active={true}>Burger Builder</NavigationItem>
-    <NavigationItem link={'/'} active={false}>Checkout</NavigationItem>
+    <NavigationItem link={'/'} exact >Burger Builder</NavigationItem>
+    <NavigationItem link={'/my-orders'} >Checkout</NavigationItem>
   </ul>
 )
 
