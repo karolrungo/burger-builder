ca0938380d4c96d47790911ed77c62e7c4eeaa32
rungo <karol.rungo@nokia.com>
2018-08-23 13:33:43 +0200

    introduce failing ut for fetchOrder action

---
+++ src/store/actions/order.test.js
@@ -0,0 +1,51 @@
+import moxios from 'moxios';
+import thunk from 'redux-thunk';
+
+import configureMockStore from 'redux-mock-store';
+import * as actionTypes from './actionTypes';
+import * as actions from './order'
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+const ordersMock = {
+  "-LGyxbZUSr5Q4jboj0uw" : {
+    "ingredients" : {
+      "bacon" : 0,
+      "cheese" : 0,
+      "meat" : 1,
+      "salad" : 0
+    },
+  }
+}
+
+describe('order actions', () => {
+  beforeEach(function () {
+    moxios.install();
+  });
+
+  afterEach(function () {
+    moxios.uninstall();
+  });
+
+    it('creates FETCH_ORDER_SUCCESS after successfuly fetching orders', () => {
+      moxios.wait(() => {
+        const request = moxios.requests.mostRecent();
+        request.respondWith({
+          status: 200,
+          response: ordersMock,
+        });
+      });
+
+    const expectedActions = [
+      { type: actionTypes.FETCH_ORDERS_START },
+      { type: actionTypes.FETCH_ORDERS_SUCCESS, orders: ordersMock },
+    ];
+
+      const store = mockStore({ posts: {} })
+
+      store.dispatch(actions.fetchOrders("TOKEN", "USER_ID"))
+      console.log(store.getActions())
+      expect(store.getActions()).toEqual(expectedActions);
+    })
+})
